924a44a1fe6b5b38a55dba59bbcf9b9d94e3bae9
yunusfirat <yunusfirat44@gmail.com>
2021-06-10 00:24:20 +0100

    log hello World

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
+const PORT =  process.env.PORT || 3000;
 
 
+console.log("heloo")
 
-
-
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
 
